e808634840211fb67dd91c6fc9d7adc286a221d6
SohanLal <sohanlalgupta022@gmail.com>
2020-08-01 10:41:53 +0530

    added socre & round

--- guessingGame.js
+++ guessingGame.js
@@ -5,8 +5,12 @@ var hard = document.querySelector("#hard");
 var result = document.querySelector("#result");
 var squares = document.querySelectorAll(".square");
 var h1 = document.querySelector("h1");
+var roundNum = document.querySelector("#roundNum");
+var scoreVal = document.querySelector("#scoreVal");
 
-var num = 6;
+var num = 8;
+var rndNum = 1;
+var scrVal = 0;
 var disColor = generateRandomColor();
 randomColor.textContent = disColor;
 hard.classList.add("selects");
@@ -46,8 +50,14 @@ for(var i = 0; i < squares.length; ++i){
 	squares[i].addEventListener("click", function(){
 		if(disColor === this.style.background){
 			h1.style.background = disColor;
-			result.textContent = "Correct!";
-			newColors.textContent = "PLAY AGAIN?";
+			result.textContent = "CORRECT";
+			newColors.textContent = "PLAY AGAIN";
+			for(var i = 0; i < squares.length; ++i){
+				if(squares[i].style.background !== "rgb(35, 35, 35)"){
+					scrVal++;
+				}
+			}
+			scoreVal.textContent = scrVal;
 			for(var k = 0; k < squares.length; ++k){
 				if(k >= num){
 					squares.style.background = "#232323";
@@ -59,7 +69,7 @@ for(var i = 0; i < squares.length; ++i){
 		}
 		else{
 			this.style.background = "#232323";
-			result.textContent = "Try Again";
+			result.textContent = "TRY AGAIN";
 		}
 	})
 }
@@ -71,17 +81,19 @@ function reset(){
 	generateSixColor();
 	result.textContent = "";
 	newColors.textContent = "NEW COLORS";
+	rndNum++;
+	roundNum.textContent = rndNum;
 }
 
 hard.addEventListener("click", function(){
-	num = 6;
+	num = 8;
 	easy.classList.remove("selects");
 	hard.classList.add("selects");
 	reset();
 })
 
 easy.addEventListener("click",function(){
-	num = 3;
+	num = 4;
 	easy.classList.add("selects");
 	hard.classList.remove("selects");
 	reset();
